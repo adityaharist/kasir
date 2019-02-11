a5ca2a032a6f4359a16e4932166c7e40aaf072e9
arifirmansyah <jumphigher19.com>
2019-02-11 07:36:45 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-var getRandom = function getRandom() {return Math.floor(Math.random() * 10);};
-var createNoteLocations = function createNoteLocations() {
-  var loc = [];
-  for (var i = 0; i < 10; i++) {if (window.CP.shouldStopExecution(0)) break;
+const getRandom = () => Math.floor(Math.random() * 10);
+const createNoteLocations = () => {
+  const loc = [];
+  for (let i = 0; i < 10; i++) {
     loc.push([
-    Math.floor(Math.random() * 100) - 50,
-    Math.floor(Math.random() * 300) + 150,
-    Math.floor(Math.random() * 90) - 90]);
-
-  }window.CP.exitedLoop(0);
+      Math.floor(Math.random() * 100) - 50,
+      Math.floor(Math.random() * 300) + 150,
+      Math.floor(Math.random() * 90) - 90
+    ]);
+  }
   return loc;
 };
-var app = new Vue({
+const app = new Vue({
   el: ".cash-register",
   data: {
     subbing: false,
@@ -22,35 +22,35 @@ var app = new Vue({
     notes: createNoteLocations(),
     tending: false,
     registerNoise: new Audio(
-    "https://freesound.org/data/previews/184/184438_850742-lq.mp3") },
-
-
+      "https://freesound.org/data/previews/184/184438_850742-lq.mp3"
+    )
+  },
   methods: {
-    addToLine: function addToLine(e) {
+    addToLine: function(e) {
       this.temp[e.target.getAttribute("data-amount-index")] = getRandom();
     },
-    addReceiptItem: function addReceiptItem() {var _this = this;var lines = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+    addReceiptItem: function(lines = 1) {
       this.amount = this.temp;
       this.temp = [0, 0, 0, 0];
       this.lines = this.lines + 1;
       this.receiptLines = this.receiptLines + lines;
-      this.$nextTick(function () {
-        _this.showLines = _this.showLines + lines;
+      this.$nextTick(() => {
+        this.showLines = this.showLines + lines;
       });
     },
-    addLine: function addLine() {
+    addLine: function() {
       this.addReceiptItem();
     },
-    addSub: function addSub() {
+    addSub: function() {
       this.addReceiptItem(2);
     },
-    tend: function tend(e) {
+    tend: function(e) {
       if (
-      this.tending === false &&
-      e.target.className.indexOf("front") !== -1 ||
-      this.tending && e.target.className.indexOf("tend") !== -1)
-
-      return;
+        (this.tending === false &&
+          e.target.className.indexOf("front") !== -1) ||
+        (this.tending && e.target.className.indexOf("tend") !== -1)
+      )
+        return;
       this.tending = !this.tending;
       if (this.tending) {
         this.registerNoise.play();
@@ -58,4 +58,6 @@ var app = new Vue({
       if (this.tending === false) this.registerNoise.pause();
       this.registerNoise.currentTime = 0;
       this.notes = createNoteLocations();
-    } } });
+    }
+  }
+});
